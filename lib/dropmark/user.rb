5ab14416296d5181ad78d41d8299e7fe6dc3838c
Skylar Challand <skylar@oakmade.com>
2013-09-16 13:26:25 -0400

    user validations

--- lib/dropmark/user.rb
+++ lib/dropmark/user.rb
@@ -3,11 +3,17 @@ module Dropmark
     include Her::Model
     uses_api Dropmark.api
     
+    attributes :name, :email, :password, :username
+    validates :name, presence: true
+    validates :email, format: { with: /\A([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}\z/ix }
+    validates :password, length: { minimum: 6 }
+    validates :username, format: { with: /\A(?!-)([a-z0-9-]{0,49}[a-z0-9])\z/i }
+    
     has_many :collections
     custom_get :contacts
     
     def request_path
-      if self.try(:id) == Dropmark.user_id
+      if !Dropmark.user_id.nil? and self.try(:id) == Dropmark.user_id
         "users/me"
       else
         super

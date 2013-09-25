e68d9ce43ad36ef3e3fcefb60d02cfa459a72eb5
Skylar Challand <skylar@oakmade.com>
2013-09-25 16:32:38 -0400

    add filename

--- lib/dropmark/file.rb
+++ lib/dropmark/file.rb
@@ -2,11 +2,11 @@ require 'mime/types'
 
 module Dropmark
   class File < Faraday::UploadIO
-    def initialize(file, mime = nil)
+    def initialize(file, mime = nil, filename = nil)
       if mime.nil? and detected_mimes = MIME::Types.type_for(file)
         mime = detected_mimes.first
       end
-      super(file, mime.to_s)
+      super(file, mime.to_s, filename)
     end
   end
 end

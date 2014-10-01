6ac267546cea894bbe01ba10e405de46b4e26aad
Sawyer Hollenshead <s.hollenshead@gmail.com>
2014-10-01 11:21:52 -0400

    has_many items

--- lib/dropmark/tag.rb
+++ lib/dropmark/tag.rb
@@ -2,15 +2,6 @@ module Dropmark
   class Tag < Model
     include Her::Model
     uses_api Dropmark.api
-
-    belongs_to :item
-
-    after_find do |i|
-      begin
-        i.created_at = Time.parse(i.created_at)
-        # i.updated_at = Time.parse(i.updated_at)
-      rescue
-      end
-    end
+    has_many :items
   end
 end

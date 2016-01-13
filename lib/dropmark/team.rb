9be915878ea5e9703f2f0971e00e8373953e3b37
Sawyer Hollenshead <s.hollenshead@gmail.com>
2016-01-13 13:41:53 -0500

    Add support for metadata on teams

--- lib/dropmark/team.rb
+++ lib/dropmark/team.rb
@@ -4,5 +4,10 @@ module Dropmark
     uses_api Dropmark.api
     has_many :collections
     has_many :users
+    store_metadata :_metadata # conflicted with actual user metadata
+
+    after_find do |i|
+      i.metadata = i._metadata unless i.has_attribute?('metadata')
+    end
   end
 end

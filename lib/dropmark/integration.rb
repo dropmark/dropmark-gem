7c6d2c904e1db3e89dcce26b10f4b02d07390105
Sawyer Hollenshead <s.hollenshead@gmail.com>
2016-11-08 14:05:30 -0500

    Fix integration metadata

--- lib/dropmark/integration.rb
+++ lib/dropmark/integration.rb
@@ -4,5 +4,11 @@ module Dropmark
     uses_api Dropmark.api
     belongs_to :collection
     belongs_to :user
+
+    store_metadata :_metadata # conflicted with actual integration metadata
+
+    after_initialize do |i|
+      i.metadata = i._metadata unless i.has_attribute?('metadata')
+    end
   end
 end
